de296e93e78aa9ce6660b10953d9eb0ecdbf9bf2
mhammeDev <mhammedvipp@gmail.com>
2025-04-17 11:41:19 +0200

    Harmonization

---
+++ public/js/navbar.js
@@ -0,0 +1,26 @@
+function displaySelectMenu() {
+    const valSelect = document.getElementById('additional-roles');
+    valSelect.style.display = valSelect.style.display === 'block' ? 'none' : 'block';
+}
+
+function toggleProfileMenu() {
+    const profileMenu = document.getElementById('profile-menu');
+    profileMenu.style.display = profileMenu.style.display === 'block' ? 'none' : 'block';
+}
+
+// Pour cancel le menu si il n'est pas cliqué
+document.addEventListener('click', function (event) {
+    const profileMenu = document.getElementById('profile-menu');
+    const profilePicture = document.getElementById('admin-picture');
+
+    const valSelect = document.getElementById('additional-roles');
+    const roles = document.getElementById('roles-changes');
+
+    if (event.target !== profilePicture && !profilePicture.contains(event.target)) {
+        profileMenu.style.display = 'none';
+    }
+
+    if (event.target !== roles && !valSelect.contains(event.target)) {
+        valSelect.style.display = 'none';
+    }
+});
---
+++ public/js/sidebar.js
@@ -0,0 +1,25 @@
+function toggleNav() {
+    const nav = document.getElementById('navbar');
+
+    const content = document.getElementById('content');
+    const chevron = document.getElementById('chevron');
+    const navTexts = document.querySelectorAll('.nav-text');
+    const logoText = document.querySelector('.logo-text');
+
+    if (nav.style.width === '350px' || nav.style.width !== '100px') {
+        nav.style.width = '100px';
+        nav.style.alignItems = 'center';
+        content.style.marginLeft = '150px';
+        chevron.style.transform = 'rotate(0deg)';
+        navTexts.forEach(text => text.style.display = 'none');
+        logoText.style.display = 'none';
+    } else {
+        nav.style.alignItems = 'normal';
+        nav.style.width = '350px';
+        content.style.marginLeft = '400px';
+        chevron.style.transform = 'rotate(180deg)';
+
+        navTexts.forEach(text => text.style.display = 'inline');
+        logoText.style.display = 'block';
+    }
+}
