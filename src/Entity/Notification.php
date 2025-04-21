3a882d93b393b6c6bee9aa9f4bd3b7e13cf7251d
mhammeDev <mhammedvipp@gmail.com>
2025-04-21 14:29:01 +0200

    Updating script

--- src/Entity/Notification.php
+++ src/Entity/Notification.php
@@ -14,7 +14,7 @@ class Notification
     #[ORM\GeneratedValue(strategy: "IDENTITY")]
     private ?int $id = null;
 
-    #[ORM\Column(length: 255)]
+    #[ORM\Column(type: 'text')]
     private ?string $contenu = null;
 
     #[ORM\Column(type: Types::DATETIME_MUTABLE)]
